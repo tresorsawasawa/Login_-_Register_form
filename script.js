02823b18359702374a58f5fca0c8a2a9c91684b9
tresorsawasawa <tresorsawasawa@gmail.com>
2021-09-30 19:36:03 +0200

    initial repo

--- script.js
+++ script.js
@@ -3,10 +3,22 @@
 const loginBtn = document.querySelectorAll(".login-btn"),
       registerBtn = document.querySelectorAll(".register-btn"),
       lossPassBtn = document.querySelectorAll(".lost-pass-btn"),
-      box = document.querySelector(".box")
+      box = document.querySelector(".box"),
+      loginForm = document.querySelector(".login-form"), 
+      registerForm = document.querySelector(".register-form"),
+      lostPasswordForm = document.querySelector(".lost-password-form")
 
 registerBtn.forEach((btn) =>{
     btn.addEventListener("click",() =>{
         box.classList.add("slide-active");
+        registerForm.classList.remove("form-hidden");
+        loginForm.classList.add("form-hidden");
     });
-})
+});
+loginBtn.forEach((btn) =>{
+    btn.addEventListener("click",() =>{
+        box.classList.remove("slide-active");
+        registerForm.classList.add("form-hidden");
+        loginForm.classList.remove("form-hidden");
+    });
+});
